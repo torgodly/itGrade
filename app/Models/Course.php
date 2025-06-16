3124ea95c138e305dbc4a8785e5a7fd0ba2df3bd
Abdullah Alhaj <torgodly@gmail.com>
2025-06-16 23:58:55 +0200

    add relationships between Course and Exam models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Course extends Model
 {
     /** @use HasFactory<\Database\Factories\CourseFactory> */
     use HasFactory;
+
+    public function exam(): HasOne
+    {
+        return $this->hasOne(Exam::class);
+    }
 }
--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
@@ -18,5 +19,10 @@ class Exam extends Model implements HasMedia
         return [
             'correct_answers' => 'array',
         ];
+    }
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
     }
 }
