ec0d25270f46b94426afcd54a9d6538fbc7f5327
Abdullah Alhaj <torgodly@gmail.com>
2025-06-16 23:44:23 +0200

    first commit + added course model

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(2),
+            'term' => $this->faker->randomElement(['ربيع', 'خريف', 'صيف']),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
